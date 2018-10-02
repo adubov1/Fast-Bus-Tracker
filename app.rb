bb3ea76f3d1951577107a0a06aed61815ee9fb41
Alex Dubov <adubov@cps.edu>
2018-10-02 15:28:31 -0500

    addded 22-deming cta tracker

--- app.rb
+++ app.rb
@@ -1,10 +1,14 @@
 require "sinatra"
 require 'sinatra/activerecord'
+require 'httparty'
+require 'json'
 
 class App < Sinatra::Base
   
   get "/" do
-    erb :index
+  	@twentytwodem = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=22&stpid=1836&format=json')
+    erb :index :twentytwodem
+
   end
 
 end
