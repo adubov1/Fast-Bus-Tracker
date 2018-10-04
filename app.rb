a2ba941f6ac819cf5b61951fa643285b703cc56c
Alex Dubov <adubov@cps.edu>
2018-10-03 23:44:00 -0500

    added division stops

--- app.rb
+++ app.rb
@@ -9,6 +9,8 @@ class App < Sinatra::Base
   get "/" do
   	@ttdem = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=22&stpid=1836&format=json')
   	@tsdem = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=36&stpid=1836&format=json')
+  	@ttdiv = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=22&stpid=1899&format=json')
+  	@tsdiv = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=36&stpid=5395&format=json')
     erb :index
   end
 
