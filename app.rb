b6e02a50d0c5fc0d3b808ce36e884a91430c102a
Alex Dubov <adubov@cps.edu>
2018-10-05 16:21:20 -0500

    added better logic, moved key

--- app.rb
+++ app.rb
@@ -1,17 +1,25 @@
-require "sinatra"
+require 'sinatra'
 require 'sinatra/activerecord'
 require 'httparty'
 require 'json'
-require "sinatra/reloader" if development?
+require 'sinatra/reloader' if development?
+require 'yaml'
 
 class App < Sinatra::Base
 
-  get "/" do
-  	@ttdem = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=22&stpid=1836&format=json')
-  	@tsdem = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=36&stpid=1836&format=json')
-  	@ttdiv = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=22&stpid=1899&format=json')
-  	@tsdiv = HTTParty.get('http://ctabustracker.com/bustime/api/v2/getpredictions?key=uaAwsh6vFzfc4nc66z6YR6DNQ&rt=36&stpid=5395&format=json')
-    erb :index
-  end
+	def getRoutes
+		cta_key = YAML.load(File.read("secret/cta_key.yml"))["key"]
+		@ttdem = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
+		@tsdem = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
+		@ttdiv = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
+		@tsdiv = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
+		
+	end
+
+
+	get "/" do
+		getRoutes
+		erb :index
+	end
 
 end
