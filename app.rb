b3d465f05fbb8508b9c1dbb7c021683df5f6b5ae
Alex Dubov <adubov@cps.edu>
2018-10-06 18:21:11 -0500

    fix for timezone

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ class App < Sinatra::Base
 
 	def getRoutes
 		cta_key = YAML.load(File.read("secret/cta_key.yml"))["key"]
-		@ttdem = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
-		@tsdem = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
-		@ttdiv = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
-		@tsdiv = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
+		@ttdem = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60
+		@tsdem = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60
+		@ttdiv = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60
+		@tsdiv = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60
 	end
 
 
