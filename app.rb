c517dde4b6575f50b41dcfedc132a6c2dc283541
Alex Dubov <adubov@cps.edu>
2018-10-06 17:24:20 -0500

    moved more logic to ruby, improved IOS compatibility

--- app.rb
+++ app.rb
@@ -9,11 +9,10 @@ class App < Sinatra::Base
 
 	def getRoutes
 		cta_key = YAML.load(File.read("secret/cta_key.yml"))["key"]
-		@ttdem = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
-		@tsdem = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
-		@ttdiv = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
-		@tsdiv = HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"].insert(4," ").insert(7," ").insert(10," ")
-		
+		@ttdem = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
+		@tsdem = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
+		@ttdiv = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
+		@tsdiv = (Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.to_f)/60
 	end
 
 
