5c38f92ed65ef88a259f9cda778f4cd9c96e49c8
Alex Dubov <adubov@cps.edu>
2018-10-17 00:44:24 -0500

    server error fix

--- app.rb
+++ app.rb
@@ -9,10 +9,10 @@ class App < Sinatra::Base
 
 	def getRoutes
 		cta_key = YAML.load(File.read("secret/cta_key.yml"))["key"]
-		@ttdem = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300
-		@tsdem = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300
-		@ttdiv = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300
-		@tsdiv = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300
+		@ttdem = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300 rescue nil
+		@tsdem = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=1836&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300 rescue nil
+		@ttdiv = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=22&stpid=1899&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300 rescue nil
+		@tsdiv = ((Time.parse(HTTParty.get("http://ctabustracker.com/bustime/api/v2/getpredictions?key=#{cta_key}&rt=36&stpid=5395&format=json")["bustime-response"]["prd"][0]["prdtm"]).to_f-Time.now.getlocal('-05:00').to_f)/60)+300 rescue nil
 	end
 
 
